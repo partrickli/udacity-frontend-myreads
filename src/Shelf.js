844cf8a19093983d210532030ca330cadfaabb7d
李桂炎 <partrick.li@gmail.com>
2018-11-11 00:03:25 +0800

    feat: Add readable header for shelf

--- src/Shelf.js
+++ src/Shelf.js
@@ -2,11 +2,17 @@ import React from 'react';
 import './Shelf.css';
 import Book from './Book';
 
+const shelfHeaders = {
+  read: 'Read',
+  currentlyReading: 'Currently Reading',
+  wantToRead: 'Want To Read',
+};
+
 function Shelf(props) {
   return (
     <div className="Shelf">
       <div className="shelf-header">
-        <h2>{props.shelf}</h2>
+        <h2>{shelfHeaders[props.shelf]}</h2>
       </div>
       {props.books.map((book) => {
         return <Book book={book} key={book.title} />;
