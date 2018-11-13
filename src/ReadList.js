5bbeed31c67baa6ab6f9482c6405124f38684216
李桂炎 <partrick.li@gmail.com>
2018-11-13 23:12:43 +0800

    refactor: Change ReadList to class based component

--- src/ReadList.js
+++ src/ReadList.js
@@ -1,25 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './ReadList.css';
 import Shelf from './Shelf';
 import { Link } from 'react-router-dom';
-function BookList(props) {
-  return (
-    <div className="shelves">
-      {props.shelves.map((shelf) => {
-        return (
-          <Shelf
-            key={shelf.catagory}
-            shelf={shelf.catagory}
-            books={shelf.books}
-            changeShelf={props.changeShelf}
-          />
-        );
-      })}
-      <button className="add-book">
-        <Link to="/search">Add Book</Link>
-      </button>
-    </div>
-  );
+class BookList extends Component {
+  shouldComponentUpdate() {
+    console.log('ReadList shouldComponentUpdate called');
+    return true;
+  }
+
+  render() {
+    return (
+      <div className="shelves">
+        {this.props.shelves.map((shelf) => {
+          return (
+            <Shelf
+              key={shelf.catagory}
+              shelf={shelf.catagory}
+              books={shelf.books}
+              changeShelf={this.props.changeShelf}
+            />
+          );
+        })}
+        <button className="add-book">
+          <Link to="/search">Add Book</Link>
+        </button>
+      </div>
+    );
+  }
 }
 
 export default BookList;
